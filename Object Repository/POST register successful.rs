476495cd36e88632ac8537ff2819e826151ad487
Nova <Nova@DESKTOP-NI9B1ID>
2023-09-06 09:09:16 +0700

    Update

--- Object Repository/POST register successful.rs
+++ Object Repository/POST register successful.rs
@@ -57,6 +57,9 @@ import internal.GlobalVariable as GlobalVariable
 RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 
 ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
-</verificationScript>
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
